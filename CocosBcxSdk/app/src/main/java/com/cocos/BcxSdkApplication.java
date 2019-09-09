1fa7a288f5f56326c20f02827b6e5b4a914038c4
gnkhandsome <1456448257@qq.com>
2019-09-09 10:25:22 +0800

    feat (BcxSdkApplication)  初始化工具类

--- CocosBcxSdk/app/src/main/java/com/cocos/BcxSdkApplication.java
+++ CocosBcxSdk/app/src/main/java/com/cocos/BcxSdkApplication.java
@@ -6,6 +6,7 @@ import android.util.Log;
 
 import com.cocos.bcx_sdk.bcx_api.CocosBcxApiWrapper;
 import com.cocos.bcx_sdk.bcx_callback.IBcxCallBack;
+import com.cocos.bcxsdk.utils.Utils;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,6 +22,7 @@ public class BcxSdkApplication extends Application {
     public void onCreate() {
         super.onCreate();
         MultiDex.install(this);
+        Utils.init(this);
         //初始化工具类
         List<String> mListNode = Arrays.asList("ws://39.106.126.54:8049", "ws://39.106.126.54:8049");
         String faucetUrl = "http://47.93.62.96:8041";
@@ -36,6 +38,5 @@ public class BcxSdkApplication extends Application {
                     }
                 });
     }
-
 
 }
