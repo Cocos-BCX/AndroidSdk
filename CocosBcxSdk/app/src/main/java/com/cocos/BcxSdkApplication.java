a7ff1674d9e360403ca680cae8180480f01f90c2
gnkhandsome <1456448257@qq.com>
2019-11-08 14:55:04 +0800

    fix （BcxSdkApplication） modify chain rpc

--- CocosBcxSdk/app/src/main/java/com/cocos/BcxSdkApplication.java
+++ CocosBcxSdk/app/src/main/java/com/cocos/BcxSdkApplication.java
@@ -24,9 +24,9 @@ public class BcxSdkApplication extends Application {
         MultiDex.install(this);
         Utils.init(this);
         //初始化工具类
-        List<String> mListNode = Arrays.asList("ws://182.92.164.121:8021", "ws://182.92.164.121:8021");
+        List<String> mListNode = Arrays.asList("ws://123.57.19.148:9049", "ws://123.57.19.148:9049");
         String faucetUrl = "http://47.93.62.96:8041";
-        String chainId = "0feb05b09945f0e17d022eee831873c867e30cbc2fe9bb040a91caff12713424";
+        String chainId = "9e0ef9444fc780fa91aaef2e63c18532634ad67dcc436a4b4915d3adeef62c62";
         String coreAsset = "COCOS";
         boolean isOpenLog = true;
         CocosBcxApiWrapper.getBcxInstance().init(this);
